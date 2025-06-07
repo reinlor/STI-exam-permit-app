d3e30fe1623233cad904c13be6fe116c1467051b
Rehneil Lor <raykiller456@gmail.com>
2025-06-07 15:27:23 +0800

    import updates

--- examPermit/src/admin/user/Users.jsx
+++ examPermit/src/admin/user/Users.jsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import userStyle from "./userStyle.module.css";
 import defaultProfilePic from "../../assets/profile/placeholder.png";
 
-import StudentInformation from "./studentInformation";
-import Credentials from "./Credentials";
-import BalanceInfo from "./BalanceInfo";
+import StudentInformation from "./studentInformation.jsx";
+import Credentials from "./Credentials.jsx";
+import BalanceInfo from "./BalanceInfo.jsx";
 
 function Users() {
     const [students, setStudents] = useState([]);
